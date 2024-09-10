a3e675e5c36a1864a4dafb9ab4a7b8dd149eb82e
Mhotep1889 <felipe1889@hotmail.com>
2024-09-09 21:21:32 -0500

    Add files via upload

---
+++ ejercicio5/ejercicio5taller2.py
@@ -0,0 +1,4 @@
+salarioMinimo = 1300000
+aumentoSalario = salarioMinimo*4/100
+
+print(f"El aumento del salario minimo para el proximo año es {salarioMinimo+aumentoSalario}")
---
+++ ejercicio5/ejercicio6taller2.py
@@ -0,0 +1,4 @@
+pesos=float(input("Escriba su numero en pesos"))
+peso= 4181
+
+print(f"son iguales a {pesos/peso:.2f} dolares")
