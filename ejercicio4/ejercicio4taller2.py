5f650e2ea66b2ef08a4e58e34e521f23e06807ee
Mhotep1889 <felipe1889@hotmail.com>
2024-09-10 19:02:54 -0500

    Add files via upload

---
+++ ejercicio4/ejercicio4taller2.py
@@ -0,0 +1,3 @@
+metros = float(input("Escriba su numero en metros"))
+milla = metros * 0.000621371
+print(f"Su numero en millas es{milla} millas")
---
+++ ejercicio7/ejercicio7taller2.py
@@ -0,0 +1,11 @@
+taller1=float(input("Ingrese la nota 1"))
+taller2=float(input("Ingrese la nota 2"))
+cuestionario1=float(input("Ingrese la nota 3"))
+cuestionario2=float(input("Ingrese la nota 4"))
+proyectoFinal=float(input("Ingrese la nota 5"))
+
+notaFinal=(taller1+taller2+cuestionario1+cuestionario2+proyectoFinal)/5
+
+if taller1 == 
+
+
